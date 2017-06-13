fecf14039b5b04db3faf17e730de8f9dc318f811
Alec Johnson <alecjohnson55@gmail.com>
2017-06-13 12:46:08 -0700

    update entry file, add calculator component skeleton

---
+++ calculator/calculator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
